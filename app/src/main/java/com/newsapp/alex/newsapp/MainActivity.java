8fe3cd7295945bee0e4f7119719c49daad271a68
Alexander <mburukungu@gmail.com>
2017-09-08 12:32:35 +0300

    placed a member varable to hold our button

--- app/src/main/java/com/newsapp/alex/newsapp/MainActivity.java
+++ app/src/main/java/com/newsapp/alex/newsapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.newsapp.alex.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindNewsCategoriesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
