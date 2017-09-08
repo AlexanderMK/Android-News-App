7ca549965df14e6eaee591257a30f67d8ad77883
Alexander <mburukungu@gmail.com>
2017-09-08 15:27:16 +0300

    set a new member variable for EditText

--- app/src/main/java/com/newsapp/alex/newsapp/MainActivity.java
+++ app/src/main/java/com/newsapp/alex/newsapp/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindNewsCategoriesButton;
+    private EditText mCountryEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                startActivity(intent);
             }
         });
     }
